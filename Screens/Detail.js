bef6afd7f7a7384b9befdaad71c496da38ecc557
sirdanke <ahmad.syukron6991@gmail.com>
2019-04-24 08:29:17 +0700

    update camera

--- Screens/Detail.js
+++ Screens/Detail.js
@@ -71,24 +71,24 @@ export default class Detail extends Component {
 
             <Query query={getGarbages} variables={{ token: this.state.token }}>{
                 ({ loading, error, data }) => {
-                    if (loading) return loading
+                    if (loading) return <Text>loading</Text>
                     if (error) return error
                     if (data) return (
-                        <>
-                            <MapView
-                                style={{ flex: 1, maxHeight: height * 0.9, width: width * 1 }}
-                                provider={PROVIDER_GOOGLE}
-                                zoomEnabled={true}
-                                scrollEnabled={true}
-                                showsScale={true}
-                                region={{
-                                    latitudeDelta: 0.1,
-                                    longitudeDelta: 0.1,
-                                    latitude: -6.259831,
-                                    longitude: 106.782795,
-                                }}
-                            >
-                                {
+            <>
+                <MapView
+                    style={{ flex: 1, maxHeight: height * 0.9, width: width * 1 }}
+                    provider={PROVIDER_GOOGLE}
+                    zoomEnabled={true}
+                    scrollEnabled={true}
+                    showsScale={true}
+                    region={{
+                        latitudeDelta: 0.1,
+                        longitudeDelta: 0.1,
+                        latitude: -6.259831,
+                        longitude: 106.782795,
+                    }}
+                >
+                    {
                                     data.garbages.map(e => (
                                         <Marker
                                             pinColor={e.color}
@@ -101,61 +101,74 @@ export default class Detail extends Component {
                                         </Marker>
                                     ))
                                 }
-                            </MapView>
-                            {/* <MapNavigation/> */}
-                            <View>
-                                <TouchableOpacity
-                                    style={{ width: width / 5 }}
-                                    onPress={() => this.props.navigation.navigate('Maps')}
-                                ><Text>all</Text></TouchableOpacity>
-                                <TouchableOpacity
-                                    style={{ width: width / 5 }}
-                                    onPress={() => this.props.navigation.navigate('MetalMap')}
-                                ><Text>metal</Text></TouchableOpacity>
-                                <TouchableOpacity
-                                    style={{ width: width / 5 }}
-                                    onPress={() => this.props.navigation.navigate('PaperMap')}
-                                >
-                                    <Text>paper</Text>
-                                </TouchableOpacity>
-                                <TouchableOpacity
-                                    style={{ width: width / 5 }}
-                                    onPress={() => this.props.navigation.navigate('PlasticMap')}
-                                >
-                                    <Text>plastic</Text>
-                                </TouchableOpacity>
-                                <TouchableOpacity
-                                    style={{ width: width / 5 }}
-                                    onPress={() => this.props.navigation.navigate('CardboardMap')}
-                                >
-                                    <Text>cardboard</Text>
-                                </TouchableOpacity>
-                            </View>
+                </MapView>
+                {/* <MapNavigation/> */}
+                <View
+                    style={{ flex: 1, maxHeight: height * 0.05, flexDirection: "row" }}>
+                    <TouchableOpacity
+                        style={{ width: width / 6, backgroundColor: 'blue' }}
+                        onPress={() => this.props.navigation.navigate('Maps')}
+                    ><Text>all</Text></TouchableOpacity>
+                    <TouchableOpacity
+                        style={{ width: width / 6, backgroundColor: 'red' }}
+                        onPress={() => this.props.navigation.navigate('MetalMap')}
+                    ><Text>metal</Text></TouchableOpacity>
+                    <TouchableOpacity
+                        style={{ width: width / 6, backgroundColor: 'yellow' }}
+                        onPress={() => this.props.navigation.navigate('PaperMap')}
+                    >
+                        <Text>paper</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={{ width: width / 6, backgroundColor: 'green' }}
+                        onPress={() => this.props.navigation.navigate('PlasticMap')}
+                    >
+                        <Text>plastic</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={{ width: width / 6, backgroundColor: 'orange' }}
+                        onPress={() => this.props.navigation.navigate('CardboardMap')}
+                    >
+                        <Text>cardboard</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={{ width: width / 6, backgroundColor: 'skyblue' }}
+                        onPress={() => this.props.navigation.navigate('GlassMap')}
+                    >
+                        <Text>glass</Text>
+                    </TouchableOpacity>
+                </View>
+                {/* <View
+                    style={{ flex: 1, maxHeight: height * 0.05 }}>
+                    <Button
+                        title="see detail"
+                        onPress={()=> this.getDetail()} />
+                </View> */}
 
-                            <Modal
-                                animationType="slide"
-                                transparent={false}
-                                visible={this.state.modalVisible}
-                                onRequestClose={() => {
-                                    Alert.alert('Modal has been closed.');
-                                }}>
-                                <View style={{ marginTop: 22, justifyContent: 'center', alignItems: 'center' }}>
+                {/* <Modal
+                    animationType="slide"
+                    transparent={false}
+                    visible={this.state.modalVisible}
+                    onRequestClose={() => {
+                        Alert.alert('Modal has been closed.');
+                    }}>
+                    <View style={{ marginTop: 22, justifyContent: 'center', alignItems: 'center' }}>
 
 
-                                    <Text style={{ size: 30 }}> metal :  {this.state.metal}</Text>
-                                    <Text style={{ size: 30 }}> plastic :  {this.state.plastic}</Text>
-                                    <Text style={{ size: 30 }}> paper :  {this.state.paper}</Text>
-                                    <Text style={{ size: 30 }}> glass :  {this.state.glass}</Text>
-                                    <Text style={{ size: 30 }}> cardboard :  {this.state.cardboard}</Text>
-                                    <TouchableOpacity
-                                        style={{ backgroundColor: 'gold', padding: 5 }}
-                                        onPress={() => this.setState({ modalVisible: false })}>
-                                        <Text>close</Text>
-                                    </TouchableOpacity>
+                        <Text > metal :  {this.state.metal}</Text>
+                        <Text> plastic :  {this.state.plastic}</Text>
+                        <Text > paper :  {this.state.paper}</Text>
+                        <Text> glass :  {this.state.glass}</Text>
+                        <Text> cardboard :  {this.state.cardboard}</Text>
+                        <TouchableOpacity
+                            style={{ backgroundColor: 'gold', padding: 5 }}
+                            onPress={() => this.setState({ modalVisible: false })}>
+                            <Text>close</Text>
+                        </TouchableOpacity>
 
-                                </View>
-                            </Modal>
-                        </>
+                    </View>
+                </Modal> */}
+            </>
                     )
                 }}
             </Query>
